8407a44938095a8a2238697bb31dd9fc2e79746c
Yasir Ali <iamyasirtariq@gmail.com>
2026-02-22 23:10:45 +0500

    add bg for /work

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const WorkPage = () => {
+  return (
+    <div className="min-h-screen w-full bg-white dark:bg-zinc-950 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:bg-[radial-gradient(#27272a_1px,transparent_1px)] bg-size-[16px_16px]"></div>
+  );
 };
 
-export default page;
+export default WorkPage;
